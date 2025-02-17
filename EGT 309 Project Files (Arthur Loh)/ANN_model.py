03439a42a559e34e9cd9c07452584feecc301df1
AL-2202 <arthurlohhede@gmail.com>
2025-02-17 12:01:59 +0800

    Add files via upload

---
+++ EGT 309 Project Files (Arthur Loh)/ANN_model.py
@@ -0,0 +1,26 @@
+import tensorflow
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense
+
+import pandas as pd
+import logging
+
+logger = logging.getLogger(__name__)
+
+
+def ANN_train(X_train, X_test, y_train, y_test):
+    model = Sequential()
+    model.add(Dense(64,activation='relu'))
+    model.add(Dense(32,activation='relu'))
+    model.add(Dense(32,activation='relu'))
+    model.add(Dense(1,activation='linear'))
+
+    model.summary()
+
+    model.compile(optimizer='adam',loss='mse',metrics=['mae'])
+
+    history = model.fit(X_train, y_train, epochs=10, validation_data=(X_test,y_test))
+
+    model.save("model.h5")
+
+    return model
